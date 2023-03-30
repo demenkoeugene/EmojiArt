146fc2a10d979321dd6dd1425bad839f746ae069
demenkoeugene <mimosa.01-purgers@icloud.com>
2023-03-30 15:48:03 +0300

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Eugene Demenko on 30.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
